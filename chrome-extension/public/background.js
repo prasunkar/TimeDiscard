2189d7eb9848d04fb3230c6a43f8608f7b7dbd65
Stephen Le <stephensgamingguide@gmail.com>
2021-04-24 11:29:52 -0400

    Update background.js

--- chrome-extension/public/background.js
+++ chrome-extension/public/background.js
@@ -10,8 +10,19 @@ chrome.runtime.onInstalled.addListener(() => {
   let hours= [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
   let days ={"0":[],"1":[],"2":[],"3":[],"4":[],"5":[],"6":[]};
   let workSites=[];
+  let act=false;
+  chrome.storage.local.set({act});
   chrome.storage.local.set({days});
   chrome.storage.local.set({hours});
   chrome.storage.local.set({start});
   chrome.storage.local.set({workSites});
+});
+chrome.runtime.onStartup.addListener(()=>{
+    var date = new Date();
+    let prevTime=date.getSeconds() + date.getHours()*3600 + date.getMinutes()*60;
+    chrome.storage.local.set({prevTime});
+    let start=false;
+    let act=false;
+    chrome.storage.local.set({act});
+    chrome.storage.local.set({start});
 });
